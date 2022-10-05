620ed7d35e4c16569c2536b4ae57779dacd7ef1b
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-05 16:11:22 +0200

    update on Wed Oct  5 16:11:22 CEST 2022

--- 3-run.sh
+++ 3-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+cd /mnt/MargokPool/home/sup/code/zabbix-scripts
 python2 zabbix.template.export.py --url http://192.168.1.49:10211 --user='Admin' --password="$(cat .password)" --template='All'
 # TODO figure out how to download only my templates (i.e. using tag) in above --template argument. As workaround deleting other templates
 find templates -type f -not -name 'bnowakow*' -delete
 
-# foobar - to be removed
 if ! git status | grep 'nothing to commit' >/dev/null; then 
     git status
     git commit -am "update on $(date)"
