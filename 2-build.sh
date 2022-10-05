bc57c10d8edaaab87264c8a2e002684aec935fcb
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2022-10-05 15:41:26 +0200

    add helper scripts

---
+++ 2-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# https://www.reddit.com/r/zabbix/comments/gqcoj8/how_to_download_all_templates/
+python2 -m pip installpy-zabbix
+
---
+++ 3-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python2 zabbix.template.export.py --url http://192.168.1.49:10211 --user='Admin' --password="$(cat .password)" --template='All'
+# TODO figure out how to download only my templates (i.e. using tag) in above --template argument. As workaround deleting other templates
+find templates -type f -not -name 'bnowakow*' -delete
+
