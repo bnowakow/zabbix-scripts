14161d9732f5c593d57869e5856e50ece76be490
Bartek Dobrowolski-Nowakowski <dobrowolski.nowakowski@gmail.com>
2024-06-11 19:18:22 +0200

    update on Tue Jun 11 07:18:22 PM CEST 2024

--- 1-install.sh
+++ 1-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip2.py
-python2 get-pip2.py
+# add 'deb http://archive.debian.org/debian/ stretch contrib main non-free' to install in debian
+# https://www.reddit.com/r/linuxquestions/comments/1aejta0/is_there_any_way_to_get_python_2_on_debian_12/
+python2.7 get-pip2.py
 
--- 2-build.sh
+++ 2-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # https://www.reddit.com/r/zabbix/comments/gqcoj8/how_to_download_all_templates/
-python2 -m pip install py-zabbix
+python2.7 -m pip install py-zabbix
 
